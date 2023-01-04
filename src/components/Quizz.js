8cc98b371ac25ef4d24d1ca966d99088800002b9
Subhra Mukherjee <subhramukherjee560@gmail.com>
2023-01-04 21:48:05 +0530

    basic configuration is done

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -4,25 +4,41 @@ import axios from "axios";
 const Quizz = () => {
   const [allQuestions, setallQuestions] = useState([]);
   const [allAnswers, setAllanswers] = useState([]);
-  const [currentQuestion, setCureentQuestion] = useState(0);
+  const [allOptions, setAlloptions] = useState([]);
+  const [currentQuestion, setCureentQuestion] = useState(-1);
   const [skip, setSkip] = useState(0);
   const [result, setResult] = useState(0);
 
   useEffect(() => {
     axios
-      .get(`https://opentdb.com/api.php?amount=20`)
+      .get(`https://opentdb.com/api.php?amount=5`)
       .then((res) => {
-        console.table(res.data.results);
         setallQuestions(res.data.results);
-        let allOptions = res.data.results[currentQuestion]?.incorrect_answers;
-        allOptions.push(res.data.results[currentQuestion]?.correct_answer);
-        setAllanswers(allOptions);
+        setCureentQuestion(currentQuestion+1)
+      })
+      .catch((err) => {
+        console.log(err);
       });
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      allQuestions[currentQuestion]?.incorrect_answers,
+      "no api call"
+    );
+    allQuestions[currentQuestion]?.incorrect_answers.push(
+      allQuestions[currentQuestion]?.correct_answer
+    );
+    setAlloptions(allQuestions[currentQuestion]?.incorrect_answers);
   }, [currentQuestion]);
 
-  const handleClick = () => {
-    setCureentQuestion(currentQuestion + 1);
-    setSkip(skip + 1);
+  const handleClick = (type) => {
+    if (type == "next") {
+      setCureentQuestion(currentQuestion + 1);
+      setSkip(skip + 1);
+    } else {
+      setCureentQuestion(currentQuestion - 1);
+    }
   };
 
   const submitAnswer = (answer, i) => {
@@ -37,7 +53,7 @@ const Quizz = () => {
       <div>Questions your score:{result} skip:</div>
       <div className="questions_numbers">
         <div>
-          {currentQuestion + 1} of {allQuestions.length}
+          {currentQuestion+1} of {allQuestions.length}
         </div>
         <div className="questions">
           {allQuestions[currentQuestion]
@@ -45,8 +61,9 @@ const Quizz = () => {
             : "loading"}
         </div>
       </div>
-      <div className="option_container">
-        {allQuestions[currentQuestion]?.incorrect_answers.map((ans, i) => (
+      {console.log(allOptions, "allOptions")}
+       <div className="option_container">
+        {allOptions && allOptions.map((ans, i) => (
           <div
             className="q-item"
             key={i}
@@ -57,9 +74,9 @@ const Quizz = () => {
         ))}
       </div>
       <div className="button-container">
-        <button>Prev</button>
-        <button>Prev</button>
-        <button onClick={() => handleClick()}>Next</button>
+        <button onClick={() => handleClick("prev")}>Prev</button>
+        <button onClick={()=>window.location.reload()}>Quit</button>
+        <button onClick={() => handleClick("next")}>Next</button>
       </div>
     </div>
   );
